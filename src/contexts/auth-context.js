5cb3f73e25fbcfda5d0120c44aeba5acc974c691
Anujeet Swain <anujeet98@gmail.com>
2024-02-27 02:12:12 +0530

    Added Context file

---
+++ src/contexts/auth-context.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+
+const AuthContext = React.createContext({
+    isLoggedIn: false,
+    // login: () => { },
+    // logout: () => { }
+});
+
+
+export default AuthContext;
